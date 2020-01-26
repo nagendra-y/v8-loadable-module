d40316b04b5476a2ed9e4facf0f0b94930d0f126
Nagendra Y <nagynv@gmail.com>
2020-01-26 10:19:16 +0530

    Update mesibo_test.js

--- mesibo_test.js
+++ mesibo_test.js
@@ -5,6 +5,7 @@ initialize();
 function initialize(){
 	mesibo.onmessage = Mesibo_onMessage;
 	mesibo.onmessagestatus = Mesibo_onMessageStatus;
+	mesibo.onlogin = Mesibo_onLogin;
 }
 
 function test_auto_reply(message){
@@ -18,17 +19,35 @@ function test_auto_reply(message){
 
 function test_message(){
 	var message = new Message();
-	message.peer = "test_user_demo";
+	message.id = parseInt(Math.floor(2147483647*Math.random()));
+	message.aid = 6323;
+	message.refid = 0;
 	message.gid = 0;
+	message.enableReadReceipt();
+	message.enableDeliveryReceipt();
 	message.expiry = 3600;
-	message.id = parseInt(Math.floor(2147483647*Math.random())) 
-	message.data = "testMessage";
-	message.enableReadReceipt()
-	message.enableDeliveryReceipt()
 	message.sendIfOnline(true);
+	message.when = +new Date(); 
+	message.to = "test_user_demo";
+	message.from = "js_user";
+	message.data = "testMessage";
+	
 	message.send();
 	message.sendbytes();
 }
+
+function Mesibo_onMessage(message) {
+	message.when //Timestamp
+	message.data; //raw bytes
+	message.message; //string
+	
+	return mesibo.RESULT_OK; 
+}
+
+function Mesibo_onMessageStatus(message) {
+	return mesibo.RESULT_OK; 
+}
+
 
 function test_log(){}
 
@@ -48,8 +67,26 @@ function test_http(){
 	var http = new Http();
 	http.url = "https://app.mesibo.com/api.php";
 	http.post = "op=test";	
-	http.ondata = function(http) {}
 	http.contentType = 'application/xml';
+	http.header;
+	http.extraHeaders;
+	http.userAgent;
+	http.referrer;
+	http.origin;
+	http.cookie;
+	http.encoding = 'gzip';
+	http.cacheControl;
+	http.accept;
+	http.etag;
+	http.ims;
+	
+	http.connTimeout;
+	http.headerTimeout;
+	http.bodyTimeout;
+	http.totalTimeout;
+	
+	http.ondata = function(http) {}
+	
 	http.cbdata = "cbdata"
 	http.send();
 	
@@ -63,20 +100,6 @@ function test_http(){
 	tHttp.send();	
 }
 
-function Mesibo_onMessage(message) {
-	message.when //Timestamp
-	message.data; //raw bytes
-	message.message; //string
-	
-	test_auto_reply(message);
-	test_http();
-	test_socket();
-	return mesibo.RESULT_OK; 
-}
-
-function Mesibo_onMessageStatus(message) {
-	return mesibo.RESULT_OK; 
-}
 
 function Socket_onConnect(socket){
 	socket.write("SOCKET DATA");
@@ -93,9 +116,17 @@ function test_socket(){
 	mesibo.log(sock);
 	sock.host = '127.0.0.1';
 	sock.port = 65432;
+	sock.keepAlive;
+	sock.verifyHost = true;
+	sock.enableSsl = true;
 	sock.onconnect = Socket_onConnect;
 	sock.ondata = Socket_onData;
 	sock.connect();
 	return mesibo.RESULT_OK;
+}
+
+function Mesibo_onLogin(user){
+	user.online;
+	user.address;
 }
 
