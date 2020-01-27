d389c80fe97e8a5fbed9983923744ec619d98174
Nagendra Y <nagynv@gmail.com>
2020-01-27 16:25:38 +0530

    Add files via upload

--- dialogflow.js
+++ dialogflow.js
@@ -1,62 +1,52 @@
 //dialogflow.js
 
-const MESIBO_RESULT_OK          = 0 
-const MESIBO_RESULT_FAIL        = -1 
- 
-const MESIBO_RESULT_CONSUMED    = 1 ;  
-const MESIBO_RESULT_PASS        = 0 ;
+initialize();
 
-function Mesibo_onHttpResponse(cbdata, response, datalen) {
-	mesibo_log(cbdata);
-	mesibo_log(response);
-	mesibo_log(datalen);
-
-	var p = cbdata;
-	var tmp = p.to;
-	p.to = p.from;
-	p.from = tmp;
-
-	rp_log = " ---Sending response to :" + p.to + " from :" + p.from;
-	mesibo_log(rp_log);
-	var rp = JSON.parse(response); 
-	var fulfillment = rp.queryResult.fulfillmentText;
-	
-	mesibo_log(fulfillment);
-	
-	mesibo_message(p, fulfillment, fulfillment.length);
-
-	return MESIBO_RESULT_OK;
+function initialize(){
+        mesibo.onmessage = Mesibo_onMessage;
+        mesibo.onmessagestatus = Mesibo_onMessageStatus;
 }
 
-function Mesibo_onMessage(p, message, len) {
+function Mesibo_onHttpResponse(http) {
+        var rp = JSON.parse(http.responseJSON);
+        var fulfillment = rp.queryResult.fulfillmentText;
 
-	if (1){
-		p.refid = p.id;
+        var query = http.cbdata;
+        var chatbot_response = query;
+        chatbotResponse.from = query.to;
+        chatbotResponse.to = query.from;
+        chatbotResponse.message = fulfillment;
+        chatbotResponse.refid = query.id;
+        chatbotResponse.id = parseInt(Math.floor(2147483647*Math.random()));
 
-		var base_url = 
-			"https://dialogflow.googleapis.com/v2/projects/mesibo-dialogflow/agent/sessions/"
-			+ p.id + ":detectIntent";
-		var request_body = {
-			"queryInput": {
-				"text": {
-					"text": message, 
-					"languageCode" : "en" 
-				}   
-			}
-		};
+        chatbotResponse.send();
 
-		var request_options = {
-			"extra_header": "Authorization: Bearer ya29.c.Kl64B6-kkKnQebpSkK9v7EDaEl0L0esmhWf9WjC1UR4AV_Ac2fQG3Sa2vVihUqqXB0SxfrBFfstElVb1QVzJrfgQ0aordgLLp9VOg3YvPaS3RHkmmABKdsRgjqUkR1hc",
-			"content_type": "application/json"
-		};
-
-		mesibo_http(base_url, JSON.stringify(request_body), Mesibo_onHttpResponse, p, request_options);
-		return MESIBO_RESULT_CONSUMED;
-	}
-
-	return MESIBO_RESULT_PASS;
+        return MESIBO_RESULT_OK;
 }
 
-function Mesibo_onMessageStatus(p, pStatus) {
-	return MESIBO_RESULT_PASS;
+function Mesibo_onMessage(message) {
+        var dHttp = new Http();
+        dHttp.url = "https://dialogflow.googleapis.com/v2/projects/mesibo-dialogflow/agent/sessions/"
+                +message.id+ ":detectIntent";
+        var request_body = {
+                "queryInput": {
+                        "text": {
+                                "text": message.message",
+                                "languageCode" : "en"
+                        }
+                }
+        };
+        dHttp.post = JSON.stringify(request_body);
+        dHttp.ondata = Mesibo_onHttpResponse;
+        dHttp.cbdata = message;
+                dHttp.extraHeaders = "Authorization: Bearer ya29.c.Kl67B_MR8M_Odn17CquRQ_ALxfNACBs8o97wL0NmwUZSOXjVeoJkWq8KphTk80yKlnT1bVUZAn61zkym660E2DkdxOz6w1cMO5BRPhHV_0ij378huWJaIi_bdcvVN4vT";
+        dHttp.contentType = 'application/json';
+        dHttp.send();
+
+        return mesibo.RESULT_OK;
 }
+
+function Mesibo_onMessageStatus(message) {
+        return mesibo.RESULT_OK;
+}
+
